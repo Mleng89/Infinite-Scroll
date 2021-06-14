31b9870faf723dc996ed7c6e9aba11d2158c07c1
Matthew Leng <mleng89@gmail.com>
2021-06-14 18:08:57 -0400

    Edit: Deleting comments and styling page margin

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,20 +2,13 @@ import React from 'react';
 import Data from '../../data/nyc_ttp_pins.json';
 import Header from '../Header';
 import Card from '../Card';
+import styled from 'styled-components';
 /*
 //using node.js to access .json
 const data = require('../../data/nyc_ttp_pins.json');
 */
 export default function Main() {
-    // console.log('what is my data', data[2].images);
-
     console.log('do i have a dataget?', Data);
-    function loop() {
-        console.log('length of data:', Data.length);
-        Data.forEach(ele => {
-            console.log('what is each element?', ele);
-        });
-    }
 
     // function fetchData() {
     //     fetch(getData)
@@ -26,21 +19,27 @@ export default function Main() {
     return (
         <>
             <Header />
-
-            <div>
-                hi - Pins:
+            <MainWrapper>
                 <div>
-                    {Data.map(el => {
-                        return (
-                            <Card
-                                title={el.title}
-                                imgUrl={el.images.orig.url}
-                                key={el.id}
-                            />
-                        );
-                    })}
+                    hi - Pins:
+                    <div>
+                        {Data.map(el => {
+                            return (
+                                <Card
+                                    title={el.title}
+                                    imgUrl={el.images.orig.url}
+                                    key={el.id}
+                                />
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>
+            </MainWrapper>
         </>
     );
 }
+
+const MainWrapper = styled.div`
+    margin-top: 1em;
+    margin-left: 5em;
+`;
