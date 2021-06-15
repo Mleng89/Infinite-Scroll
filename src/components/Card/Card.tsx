002952e23d781e31fa1f91192c70c2ab5782c4e6
Matthew Leng <mleng89@gmail.com>
2021-06-14 20:48:11 -0400

    Edit: Styling of each pin

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
-const PinCard = styled.div`
-    display: grid;
+
+const PinWrap = styled.div`
+    display: inline-flex;
+    padding: 8px;
 `;
-const PinImage = styled.img`
-    border-radius: 50px;
-    &:hover {
-        filter: grayscale(100%);
+const PinCard = styled.div`
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    cursor: pointer;
+    width: 250px;
+    img {
+        display: flex;
+        width: 100%;
+        cursor: zoom-in;
+        border-radius: 50px;
+        object-fit: cover;
+        &:hover {
+            filter: grayscale(100%);
+        }
     }
 `;
+
 const Card = pin => {
     return (
-        <PinCard>
-            <PinImage src={pin.imgUrl} />
-            <h2>{pin.title}</h2>
-        </PinCard>
+        <PinWrap>
+            <PinCard>
+                <img src={pin.imgUrl} />
+                <h2>{pin.title}</h2>
+            </PinCard>
+        </PinWrap>
     );
 };
 
